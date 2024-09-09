052291ed9f0531a38415698429a746763705d6d3
Chendarev Mikhail <mihasya04091986@gmail.com>
2024-09-09 19:03:19 +0300

    version 1.1.0 add diameter function

--- GeometryLibrary/src/main/java/org/example/geometry/Circle.java
+++ GeometryLibrary/src/main/java/org/example/geometry/Circle.java
@@ -10,6 +10,10 @@ public class Circle {
 
     private double radius;
 
+    public double getDiameter() {
+        return radius * 2;
+    }
+
     public double getArea() {
         return Math.PI * radius * radius;
     }
