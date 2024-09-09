f4195cf2086940f41a7bbf1167fc059e8cc1ceb4
Chendarev Mikhail <mihasya04091986@gmail.com>
2024-09-09 20:32:19 +0300

    version 1.2.0-SNAPSHOT add triangle functions

--- GeometryLibrary/src/main/java/org/example/geometry/threedimensional/Cube.java
+++ GeometryLibrary/src/main/java/org/example/geometry/threedimensional/Cube.java
@@ -1,2 +1,19 @@
-package org.example.geometry.threedimensional;public class Cube {
+package org.example.geometry.threedimensional;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Cube {
+
+    private double side;
+
+    public double getVolume() {
+        return Math.pow(side, 3);
+    }
+
+    public double getSurfaceArea() {
+        return 6 * Math.pow(side, 2);
+    }
 }
